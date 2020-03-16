ba79fd363035876f71b8b50807f727f6a65a127a
Dennis <dennisweller@gmx.net>
2020-03-16 17:59:26 +0100

    Create basic class structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 class ToDoList {
-  constructor() {}
+  constructor () {
+    this._list = [];
+  }
+
+  addItem (title) {
+    this._list.push(new ToDo(title));
+  }
 }
 
 class ToDo {
-  constructor() {}
+  constructor (title) {
+    this._title = title;
+    this._isFinished = false;
+  }
+
+  finishTodo () {
+    this._isFinished = true;
+  }
 }
+
+const myList = new ToDoList();
+
+console.log(myList);
+myList.addItem('Task 1');
+myList.addItem('Task 2');
+myList.addItem('Task 3');
+myList._list[1].finishTodo();
+console.log(myList);
